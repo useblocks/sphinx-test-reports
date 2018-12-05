d0ce3d7e7e9484a0a57be63101b0a3cb3790c39b
saifsayyad <saifsayyad9@gmail.com>
2018-12-05 18:54:57 +0530

    test case for Warning

--- tests/test_env.py
+++ tests/test_env.py
@@ -1,11 +1,26 @@
 from sphinx_testing import with_app
 import re
+import sphinx
+
+sphinx_version = int(sphinx.__version__.split('.')[0] + sphinx.__version__.split('.')[1])
+
+try:
+    from pathlib import Path
+except ImportError:
+    from pathlib2 import Path
+
+try:
+    from StringIO import StringIO  # Python 2
+except ImportError:
+    from io import StringIO  # Python 3
+from subprocess import check_output, STDOUT
 
 
 @with_app(buildername='html', srcdir='doc_test/env_report_doc')
 def test_doc_env_report_build_html(app, status, warning):
     app.build()
     html = (app.outdir / 'index.html').read_text()
+
     assert '<h1>Test-Env report' in html
     assert '<th class="head">Variable</th>' in html
 
@@ -78,3 +93,50 @@ def test_doc_env_report_default_build_html(app, status, warning):
         assert 'python' in table
         assert 'installed_packages' in table
         assert 'installpkg' in table
+
+
+@with_app(buildername='html', srcdir='doc_test/env_report_warnings')  # , warningiserror=False, verbosity=2)
+def test_doc_env_report_warning_build_html(app, status, warning):
+
+    # it should pass all test cases from: test_doc_env_report_build_html
+    # generated output data will be stored here
+    output = str(check_output(["sphinx-build", "-a", "-E", "-b", "html", app.srcdir, app.outdir],
+                              stderr=STDOUT, universal_newlines=True))
+    # app.build()
+    html = (app.outdir / 'index.html').read_text()
+
+    assert '<h1>Test-Env report' in html
+    assert '<th class="head">Variable</th>' in html
+
+    tables = re.findall("(<table .*?</table>)", html, re.DOTALL)
+
+    assert len(tables) == 2
+
+    # All requested data should be in both tables
+    assert 'name' in tables[0]
+    assert 'path' in tables[0]
+    assert 'host' in tables[0]
+
+    assert 'name' in tables[1]
+    assert 'path' in tables[1]
+    assert 'host' in tables[1]
+
+    # table 0 should contain py35
+    assert 'py35' in tables[0]
+    assert 'flake8' not in tables[0]
+
+    # table 1 should contain flake8
+    assert 'flake8' in tables[1]
+    assert 'py35' not in tables[1]
+
+    """
+    Taken from solution of this issue:
+        https://github.com/useblocks/sphinxcontrib-needs/issues/44
+    """
+    
+    if sphinx_version > 15:
+        assert "WARNING: environment 'defs' is not present in JSON file" in output
+        assert "WARNING: option 'abc' is not present in JSON file" in output
+    else:
+        assert "environment 'defs' is not present in JSON file" in output
+        assert "option 'abc' is not present in JSON file" in output
