c395a6f6fa69a34c40c25ae3f9578794e4957e75
James Barr <James.Matthew.Barr@gmail.com>
2021-08-01 21:23:56 +0100

    Support added for pytest xfail test cases

--- tests/test_basic_doc.py
+++ tests/test_basic_doc.py
@@ -7,3 +7,14 @@ def test_doc_build_html(app, status, warning):
     html = (app.outdir / 'index.html').read_text()
     assert '<h1>Basic Document' in html
     assert 'ASuccessfulTest' in html
+
+
+@with_app(buildername='html', srcdir='doc_test/pytest_6_2')
+def test_doc_build_html_for_pytest_6_2(app, status, warning):
+    app.build()
+    html = (app.outdir / 'index.html').read_text(encoding="utf-8")
+
+    assert 'Tests: 6' in html
+    assert 'Failures: 2' in html
+    assert 'Errors: 0' in html
+    assert 'Skips: 3' in html
