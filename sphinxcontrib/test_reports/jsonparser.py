019810b3c02219497d5d0f6bab8b2ec565410b1f
Korbinian weber <korbinianweber@web.de>
2025-07-20 19:23:32 +0200

    Fix all mypy errors in jsonparser.py

--- sphinxcontrib/test_reports/jsonparser.py
+++ sphinxcontrib/test_reports/jsonparser.py
@@ -9,11 +9,28 @@ API must be in sync with the JUnit parser in ``junitparser.py``.
 import json
 import operator
 import os
-from functools import reduce
-from typing import Any, Dict, List
+from typing import (
+    Dict,
+    List,
+    Union,
+    Sequence,
+    Optional,
+    Tuple,
+    TypedDict,
+    cast,
+)
 
 
-def dict_get(root, items, default=None):
+class MappingEntry(TypedDict):
+    testcase: Dict[str, Tuple[Sequence[Union[str, int]], object]]
+    testsuite: Dict[str, Tuple[Sequence[Union[str, int]], object]]
+
+
+def dict_get(
+    root: Union[Dict[Union[str, int], object], List[object]],
+    items: Sequence[Union[str, int]],
+    default: Optional[object] = None
+) -> object:
     """
     Access a nested object in root by item sequence.
 
@@ -23,31 +40,48 @@ def dict_get(root, items, default=None):
 
     """
     try:
-        value = reduce(operator.getitem, items, root)
+        obj: Union[Dict[Union[str, int], object], List[object]] = root
+        for key in items:
+            next_obj = obj[key]  # type: ignore[index]
+            obj = cast(Union[Dict[Union[str, int], object], List[object]], next_obj)
+        return obj
     except (KeyError, IndexError, TypeError):
         return default
-    return value
+
 
 
 class JsonParser:
-    def __init__(self, json_path, *args, **kwargs):
+    json_path: str
+    json_data: List[Dict[str, object]]
+    json_mapping: MappingEntry
+
+    def __init__(self, json_path: str, *args: object, **kwargs: Dict[str, object]) -> None:
         self.json_path = json_path
 
         if not os.path.exists(self.json_path):
             raise JsonFileMissing(f"The given file does not exist: {self.json_path}")
 
-        self.json_data = []
-        with open(self.json_path) as jfile:
-            self.json_data = json.load(jfile)
+        with open(self.json_path, encoding="utf-8") as jfile:
+            data_raw = json.load(jfile)  # type: ignore
+            data: List[Dict[str, object]] = cast(List[Dict[str, object]], data_raw)
 
-        self.json_mapping = kwargs.get("json_mapping", {})
+        if not isinstance(data, list):
+            raise TypeError("Expected top-level JSON to be a list of dicts")
 
-    def validate(self):
+        self.json_data = data
+
+        mapping_fallback: MappingEntry = {"testcase": {}, "testsuite": {}}
+        self.json_mapping = cast(
+            MappingEntry,
+            kwargs.get("json_mapping", mapping_fallback)
+        )
+
+    def validate(self) -> bool:
         # For JSON we validate nothing here.
         # But to be compatible with the API, we need to return True
         return True
 
-    def parse(self) -> List[Dict[str, Any]]:
+    def parse(self) -> List[Dict[str, object]]:
         """
         Creates a common python list of object, no matter what information are
         supported by the parsed json file for test results junit().
@@ -55,44 +89,43 @@ class JsonParser:
         :return: list of test suites as dictionaries
         """
 
-        def parse_testcase(json_dict) -> Dict[str, Any]:
-            tc_mapping = self.json_mapping.get("testcase")
-            tc_dict = {
-                k: dict_get(json_dict, v[0], v[1]) for k, v in tc_mapping.items()
+        def parse_testcase(json_dict: Dict[Union[str, int], object]) -> Dict[str, object]:
+            tc_mapping = self.json_mapping.get("testcase", {})
+            return {
+                k: dict_get(json_dict, path, fallback)
+                for k, (path, fallback) in tc_mapping.items()
             }
-            return tc_dict
 
-        def parse_testsuite(json_dict) -> Dict[str, Any]:
-            ts_mapping = self.json_mapping.get("testsuite")
-            ts_dict = {
-                k: dict_get(json_dict, v[0], v[1])
-                for k, v in ts_mapping.items()
+        def parse_testsuite(json_dict: Dict[Union[str, int], object]) -> Dict[str, object]:
+            ts_mapping = self.json_mapping.get("testsuite", {})
+            ts_dict: Dict[str, object] = {
+                k: dict_get(json_dict, path, fallback)
+                for k, (path, fallback) in ts_mapping.items()
                 if k != "testcases"
             }
-            ts_dict.update({"testcases": [], "testsuite_nested": []})
 
-            testcases = dict_get(
-                json_dict, ts_mapping["testcases"][0], ts_mapping["testcases"][1]
-            )
-            for tc in testcases:
-                new_testcase = parse_testcase(tc)
-                ts_dict["testcases"].append(new_testcase)
+            testcases: List[Dict[str, object]] = []
+            ts_dict["testcases"] = testcases
+            ts_dict["testsuite_nested"] = []
+
+            testcase_entry = ts_mapping.get("testcases")
+            if testcase_entry:
+                testcases_raw = dict_get(json_dict, testcase_entry[0], testcase_entry[1])
+                if isinstance(testcases_raw, list):
+                    for item in testcases_raw:
+                        if isinstance(item, dict):
+                            tc = parse_testcase(item)
+                            testcases.append(tc)
 
             return ts_dict
 
         # main flow starts here
 
-        result_data = []
+        return [parse_testsuite(cast(Dict[Union[str, int], object], ts)) for ts in self.json_data]
 
-        for testsuite_data in self.json_data:
-            complete_testsuite = parse_testsuite(testsuite_data)
-            result_data.append(complete_testsuite)
-
-        return result_data
-
-    def docutils_table(self):
+    def docutils_table(self) -> None:
         pass
 
 
-class JsonFileMissing(BaseException):
+class JsonFileMissing(Exception):
     pass
